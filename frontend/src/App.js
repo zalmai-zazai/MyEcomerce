ec191054172b604a5058d0b150431daa834e9083
Zalmai-Zazai <shahzalmai98@gmail.com>
2023-07-19 15:56:43 -0700

    Created-reactApp

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">My EComerce</a>
+      </header>
+      <main>Products List</main>
+    </div>
+  );
+}
+
+export default App;
